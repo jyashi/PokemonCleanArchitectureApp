9a093c048c966e7b511a174749a1be3f6a1023da
jyashi <navtejdhot@gmail.com>
2022-10-27 00:56:09 +0900

    Fixed Text Snapping with some housekeep

--- app/src/main/java/com/example/daggerhiltexample/MainActivity.kt
+++ app/src/main/java/com/example/daggerhiltexample/MainActivity.kt
@@ -3,13 +3,8 @@ package com.example.daggerhiltexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.daggerhiltexample.navigation.NavGraph
 import com.example.daggerhiltexample.ui.theme.DaggerHiltExampleTheme
@@ -21,20 +16,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DaggerHiltExampleTheme {
-                // A surface container using the 'background' color from the theme
-
-//                    NavGraph()
-                    Column(
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        modifier = Modifier.fillMaxHeight()
-                    ) {
-                        NavGraph()
-
-                    }
-
-
-
+                NavGraph()
             }
         }
     }
