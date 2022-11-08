7318d7d38f6579d09761bea8a1ebf978754a33fc
jyashi <navtejdhot@gmail.com>
2022-11-09 08:53:34 +0900

    Adding Room integration

--- app/src/main/java/com/example/daggerhiltexample/data/domain/dummy.kt
+++
@@ -1,2 +0,0 @@
-package com.example.daggerhiltexample.data.domain
-
---
+++ app/src/main/java/com/example/daggerhiltexample/data/network/repository/PokemonDataObject.kt
@@ -0,0 +1,13 @@
+package com.example.daggerhiltexample.data.network.repository
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class PokemonDataObject(
+    @PrimaryKey(autoGenerate = false)
+    val id: Int,
+    val name: String,
+    val type: String,
+    val sprite: String,
+)
